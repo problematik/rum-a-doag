07437855c31d703c86002f6bad98e06a30a201a7
Klemen Slatnar <klemen.slatnar@gmail.com>
2021-12-02 23:13:11 +0200

    Extend HBS
    Adds partials folder
    Add helper functions
    Ensure nodemon restars on partials folder change

--- server/src/server.js
+++ server/src/server.js
@@ -2,12 +2,15 @@ import express from 'express'
 import { resolve } from 'path'
 import { router as fallbackRouter } from './routes/index.js'
 import { router as productsRouter } from './routes/products.js'
+import { init as initHbs } from './utils/view.js'
 
 const app = express()
 const port = process.env.PORT
 
 app.set('view engine', 'hbs')
 app.set('views', resolve('./views'))
+
+initHbs()
 
 app.use('/products', productsRouter)
 app.use('*', fallbackRouter)
---
+++ server/src/utils/view.js
@@ -0,0 +1,23 @@
+import hbs from 'hbs'
+import { resolve } from 'path'
+
+export function init () {
+  hbs.registerPartials(resolve('./views/partials'))
+
+  hbs.registerHelper('JSON', (value) => {
+    return JSON.stringify(value)
+  })
+
+  hbs.registerHelper('starSelected', (value, higherThan) => {
+    return value > higherThan
+  })
+}
+
+export function render (res, opts = {}) {
+  const viewOpts = {
+    ...opts
+  }
+  console.log('Rendering view', { viewOpts })
+
+  res.render('main', viewOpts)
+}
