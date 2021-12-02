132f405b662dc744ccab09e5d6f020c332fca2e1
Klemen Slatnar <klemen.slatnar@gmail.com>
2021-12-02 23:04:39 +0200

    Add live refresh

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    port: 3001
+    port: 3001,
+    fs: {
+      strict: false,
+      allow: ['..']
+    }
   }
 })
