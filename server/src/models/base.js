100516e4fb0c4931062f03796358006a51d19650
Klemen Slatnar <klemen.slatnar@gmail.com>
2021-12-02 22:32:10 +0200

    Add basic fetching method for models

--- server/src/models/base.js
+++ server/src/models/base.js
@@ -1,6 +1,10 @@
 import { db } from '../utils/db.js'
 
 export class BaseModel {
+  constructor (attrs) {
+    this.attrs = attrs
+  }
+
   static get db () {
     if (!this.tableName) { throw new Error('Child class must set tableName') }
     return db(this.tableName)
@@ -9,5 +13,27 @@ export class BaseModel {
   get db () {
     if (!this.constructor.tableName) { throw new Error('Child class must set tableName') }
     return db(this.constructor.tableName)
+  }
+
+  static _rowToClass (row) {
+    if (row && row.length) {
+      return new this(row[0])
+    }
+
+    return null
+  }
+
+  static findFirst () {
+    return this.db
+      .orderBy('createdAt', 'asc')
+      .limit(1)
+      .then(this._rowToClass.bind(this))
+  }
+
+  static findOneBy (attrs) {
+    return this.db
+      .where(attrs)
+      .limit(1)
+      .then(this._rowToClass.bind(this))
   }
 }
