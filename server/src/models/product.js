598b86f44c168edce3c84224e91c868dc0fef9ca
Klemen Slatnar <klemen.slatnar@gmail.com>
2021-12-03 02:06:08 +0200

    Add relationsip - reviews
    Calling this method will fetch the correct reviews for the product at hand

--- server/src/models/product.js
+++ server/src/models/product.js
@@ -1,5 +1,20 @@
 import { BaseModel } from './base.js'
+import { Review } from './review.js'
 
 export class Product extends BaseModel {
   static tableName = 'products'
+
+  reviews () {
+    return Review.findBy({ productId: this.attrs.id })
+  }
+
+  async getRating () {
+    const reviews = await this.reviews()
+    const ratings = reviews.reduce((memo, review) => {
+      memo += review.attrs.rating
+      return memo
+    }, 0)
+
+    return reviews.length ? Math.round((ratings / reviews.length) * 10) / 10 : 0
+  }
 }
